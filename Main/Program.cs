9a521bd2760f41f06cb7dfde3518b46c14b39020
= <=>
2025-06-16 22:10:04 +0900

    csvテスト

--- Main/Program.cs
+++ Main/Program.cs
@@ -3,6 +3,7 @@
 using System.IO;
 using System.Collections.Concurrent;
 using System.Text;
+using Common;
 
 namespace Ctest2
 {
@@ -13,7 +14,54 @@ namespace Ctest2
             Console.WriteLine("Hello, World!");
 
             var list = new List<List<sbyte>>();
-            list.Add(new List<sbyte>() { 1, 2, 3 });
+            //list.Add(new List<sbyte>() { 1, 2, 3 });
+
+            var array = new System.SByte[] {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+
+            11,
+            12,
+            13,
+            14,
+            15,
+
+            16,
+            17,
+            18,
+            19,
+            20,
+
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            29,
+            30,
+            31,
+            32,
+            33,
+            34,
+            35,
+            36,
+            37
+
+        };
+            list = array.Combination(4).ToList();
+
+            Console.WriteLine("List=" + list.Count);
             SaveCSV("./test.csv", list);
         }
 
